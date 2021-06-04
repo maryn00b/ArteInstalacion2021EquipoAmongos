07456f9af6bf0faebab0a946ff2dde32ef76df36
A01367072 <a01367072@itesm.mx>
2021-06-04 17:07:07 -0500

    inicio sistema dinero

---
+++ Assets/Scripts/Dinero.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dinero : MonoBehaviour
+{
+    public Text textoContador;
+    private int puntuacion = 10000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void restaDinero()
+    {
+        puntuacion = puntuacion - 3000;
+        textoContador.text = puntuacion.ToString();
+    }
+}
