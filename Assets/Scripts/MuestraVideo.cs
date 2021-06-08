009f69f55324d82acd24b62a28b9f06d3008ca90
A01367072 <a01367072@itesm.mx>
2021-06-08 17:06:30 -0500

    video

---
+++ Assets/Scripts/MuestraVideo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MuestraVideo : MonoBehaviour
+{
+    public string streamingVideoClip;
+    VideoPlayer player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+        player.url = Path.Combine(Application.streamingAssetsPath + streamingVideoClip);
+        player.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
