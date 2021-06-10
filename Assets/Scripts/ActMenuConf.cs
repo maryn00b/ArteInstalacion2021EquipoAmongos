e8277c08c0c9e94e2d1e2530eb02a5c313db5058
A01367072 <a01367072@itesm.mx>
2021-06-10 18:20:06 -0500

    Flash llop

---
+++ Assets/Scripts/ActMenuConf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActMenuConf : MonoBehaviour
+{
+    public GameObject menuDesplegable;
+    public void depslegarMenu()
+    {
+        if (!menuDesplegable.active)
+        {
+            menuDesplegable.SetActive(true);
+        }
+        else if (menuDesplegable.active)
+        {
+            menuDesplegable.SetActive(false);
+        }
+    }
+}
