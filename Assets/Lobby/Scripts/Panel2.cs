0ef7afefbff5ad1c72125c180cbf5d8aea42d9e3
aleA01706199 <A01706199@itesm.mx>
2021-06-11 10:51:04 -0500

    Primera escena

---
+++ Assets/Lobby/Scripts/Panel2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel2 : MonoBehaviour
+{
+
+    public GameObject panel1;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Panels.jugadorCerca && !panel1.active)
+        {
+            panel1.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Lobby/Scripts/Panels.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Panels : MonoBehaviour
+{
+    //public GameObject panel1;
+    //public GameObject comenzar;
+    public static bool jugadorCerca = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("OnTriggerEnterJugador");
+            jugadorCerca = true;
+            Debug.Log(jugadorCerca);
+        }
+
+    }
+    
+    
+}
