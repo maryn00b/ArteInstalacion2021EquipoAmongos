02846f1d67b0729b1f3902f70622a06fe97c3739
aleA01706199 <A01706199@itesm.mx>
2021-06-12 18:04:59 -0500

    Animaciones robot

--- Assets/Scripts/InteraccionesRobot.cs
+++ Assets/Scripts/InteraccionesRobot.cs
@@ -7,7 +7,7 @@ public class InteraccionesRobot : MonoBehaviour
     public  Animator personajeAnimado;
     public  Animator controlTraslacion;
     public static bool walk =false;
-    
+    public static bool chekWalk = true;
 
     void Start()
     {
@@ -17,18 +17,30 @@ public class InteraccionesRobot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
-        if (Flash.foto)
-        {
-            personajeAnimado.SetBool("walking", false);
-            personajeAnimado.SetBool("angry", true);
-        }
         if (walk)
         {
             personajeAnimado.SetBool("walking", true);
             controlTraslacion.SetBool("traslacion", true);
+            personajeAnimado.SetBool("angry", false);
+            personajeAnimado.SetBool("idle", false);
+            walk = false;
         }
+
+        if (IdleWalk.pisoCol && personajeAnimado.GetBool("walking") == true)
+        {
+            personajeAnimado.SetBool("walking", false);
+            personajeAnimado.SetBool("angry", false);
+            personajeAnimado.SetBool("idle", true);
+
+        }
+        if (Flash.foto)
+        {
+            personajeAnimado.SetBool("walking", false);
+            personajeAnimado.SetBool("angry", true);
+            personajeAnimado.SetBool("idle", false);
+        }
+       
+        
 
 
     }
---
+++ Assets/Scripts/Robot y demas/IdleWalk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleWalk : MonoBehaviour
+{
+    public static bool pisoCol=false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        if (other.CompareTag("Player"))
+        {
+            pisoCol=true;
+
+        }
+
+    }
+}
